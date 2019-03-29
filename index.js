f9997c67d92bd9dfa685e7ab7335a5d194e48aa3
chaosBreaking <x491807573@qq.com>
2019-03-29 13:45:50 +0800

    移除RC的计算，改为常量; 移除rcRes;更改index导出方式

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 const k = require('./keccak.js')
 const d = require('./derived.js')
 const h = require('./hmac-sha3.js')
-const mix = Object.assign(k, d, h)
 
-module.exports = mix
+module.exports = Object.assign(k, d, h)
